d184a6b5ea03b27f3f70daf1cf390777f822c0fa
bouargalne <bouargalne.hamid@gmail.com>
2023-10-28 23:25:48 +0100

    Tp3

---
+++ TP3/custumer-service/src/test/java/ma/bouargalne/custumerservice/CustumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bouargalne.custumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
